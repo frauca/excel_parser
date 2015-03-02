7523f3a054018dc5ae25059f82769a5ec1a0ad57
Roger Frauca <roger.frauca@gmail.com>
2015-03-02 23:57:43 +0100

    per setmanes va byWeekOv. Com et comento en un altre commit, el select year ho tens fet. Et deixo pendent de fer que quan seleccionis el year el mes tb ho pilli de base de dades segons el any

--- grails-app/services/frauca/QueryService.groovy
+++ grails-app/services/frauca/QueryService.groovy
@@ -8,47 +8,57 @@ class QueryService {
     def totalMovsAmount(params) {
 		def query =""""""
 		def ands=""
+		def pars=[]
 		if(params.ccc){
-			ands +=" and account=${params.ccc}"
+			ands +=" and account=?"
+			pars<<params.ccc
 		}
 		if(params.year){
-			ands +=" and year(valueDate)=${params.year}"
+			ands +=" and year(valueDate)=?"
+			pars<<params.year.toInteger()
 		}
 		if(params.month){
-			ands +=" and month(valueDate)=${params.month}"
+			ands +=" and month(valueDate)=?"
+			pars<<params.month.toInteger()
 		}
 		if(params.dateBigger){
-			ands +=" and valueDate>='${params.dateBigger}'"
+			ands +=" and valueDate>=?"
+			pars<<Date.parse("yyyy-MM-dd",params.dateBigger)
 		}
 		if(params.dateLower){
-			ands +=" and valueDate<='${params.dateLower}'"
+			ands +=" and valueDate<=?"
+			pars<<Date.parse("yyyy-MM-dd",params.dateLower)
 		}
-		log.info "totalAmount ${query}"
-		def res=[total:AccountMov.executeQuery("select sum(amount) from AccountMov where 1=1"+ands)[0]?.round(2)
+		if(params.week){
+			ands +=" and week(valueDate)=?"
+			pars<<params.week.toInteger()
+		}
+		log.info "totalAmount ${ands} ${pars}"
+		def res=[total:AccountMov.executeQuery("select sum(amount) from AccountMov where 1=1"+ands,pars)[0]?.round(2)
 				,ingresos:[
-						total:AccountMov.executeQuery("select sum(amount) from AccountMov where amount>=0"+ands)[0]?.round(2)
+						total:AccountMov.executeQuery("select sum(amount) from AccountMov where amount>=0"+ands,pars)[0]?.round(2)
 						,categoria:AccountMov.executeQuery("""select categoritzation.category.name,sum(amount) 
 																	from AccountMov where  amount>=0 """
-													+ands+" group by categoritzation.category.name")
+													+ands+" group by categoritzation.category.name",pars)
 						,concepte:AccountMov.executeQuery("""select categoritzation.category.name,concept,sum(amount) 
 																	from AccountMov where  amount>=0 """
-													+ands+" group by categoritzation.category.name,concept")
+													+ands+" group by categoritzation.category.name,concept",pars)
 						]
 				,gastos:[
-						total:AccountMov.executeQuery("select sum(amount) from AccountMov where amount<0"+ands)[0]?.round(2)
+						total:AccountMov.executeQuery("select sum(amount) from AccountMov where amount<0"+ands,pars)[0]?.round(2)
 						,categoria:AccountMov.executeQuery("""select categoritzation.category.name,sum(amount) 
 																	from AccountMov where  amount<0 """
-													+ands+" group by categoritzation.category.name")
+													+ands+" group by categoritzation.category.name",pars)
 						,concepte:AccountMov.executeQuery("""select categoritzation.category.name,concept,sum(amount) 
 																	from AccountMov where  amount<0 """
-													+ands+" group by categoritzation.category.name,concept")]
+													+ands+" group by categoritzation.category.name,concept",pars)]
 				,categoria:AccountMov.executeQuery("""select categoritzation.category.name,sum(amount) 
 																	from AccountMov where 1=1 """
-													+ands+" group by categoritzation.category.name")
+													+ands+" group by categoritzation.category.name",pars)
 				,concepte:AccountMov.executeQuery("""select categoritzation.category.name,concept,sum(amount)
 																	from AccountMov where 1=1 """
-													+ands+" group by categoritzation.category.name,concept")
-				,detalls:AccountMov.executeQuery("from AccountMov where amount>=0"+ands)
+													+ands+" group by categoritzation.category.name,concept",pars)
+				,detalls:AccountMov.executeQuery("from AccountMov where amount>=0"+ands,pars)
 			]
 		return res
 
